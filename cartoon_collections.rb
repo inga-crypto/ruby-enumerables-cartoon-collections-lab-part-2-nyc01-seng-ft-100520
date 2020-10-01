17318a765f8089b1b71c1c4e683857195c423396
Inga <ijaanimets@gmail.com>
2020-10-01 01:24:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,5 +24,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.select do |word|
-    if 
+    if planeteer_calls.include?(valid_calls.each)
+    end
+  end
   end
